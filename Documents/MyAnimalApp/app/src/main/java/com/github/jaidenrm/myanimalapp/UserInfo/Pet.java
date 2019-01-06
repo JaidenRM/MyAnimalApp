9146944cad7d7786c678f39299d5eed54e726956
JaidenRM <jaidenreecemuscat@gmail.com>
2019-01-06 15:45:11 +1100

    basic home screen layout created and some classes for user information

---
+++ Documents/MyAnimalApp/app/src/main/java/com/github/jaidenrm/myanimalapp/UserInfo/Pet.java
@@ -0,0 +1,10 @@
+package com.github.jaidenrm.myanimalapp.UserInfo;
+
+public class Pet {
+    private String name;
+    //private Image displayPic;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+}
---
+++ Documents/MyAnimalApp/app/src/main/java/com/github/jaidenrm/myanimalapp/UserInfo/User.java
@@ -0,0 +1,11 @@
+package com.github.jaidenrm.myanimalapp.UserInfo;
+
+public class User {
+
+    private String email;
+    private String username;
+    private Pet[] pets;
+
+    public User() {}
+
+}
